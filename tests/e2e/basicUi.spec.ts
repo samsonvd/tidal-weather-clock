efb73361980cc45d0e33df419f11daf5306a29ec
Samson Ventura-Danziger <samson@danziger.uk>
2026-05-29 18:00:05 +0100

    remove ts code

--- tests/e2e/basicUi.spec.ts
+++
@@ -1,8 +0,0 @@
-import CONSTANTS from '@/lib/constants'
-import { expect, test } from '@playwright/test'
-
-test('homepage has expected title', async ({ page }) => {
-  await page.goto('http://localhost:3000') // Your running Next.js app URL
-
-  await expect(page).toHaveTitle(CONSTANTS.TITLE)
-})
